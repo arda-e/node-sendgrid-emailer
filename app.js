5cbe475a93ec3ad936713a03207f9dcae014df2c
Meric Arda Eren <aeren@netas.com.tr>
2023-09-21 00:39:49 +0300

    Fix lint settings

--- app.js
+++ app.js
@@ -5,61 +5,59 @@ const sendEmail = require('./sendEmail');
 const program = new Command();
 
 program
-    .name('node-sendgrid-emailer')
-    .description('CLI to send HTML email to a list of recipients')
-    .version('0.0.1');
+  .name('node-sendgrid-emailer')
+  .description('CLI to send HTML email to a list of recipients')
+  .version('0.0.1');
 
-program.command('send')
-    .description('Send email to a list of recipients')
+program.command('send').description('Send email to a list of recipients');
 
 program
-    .option('-h, --html <htmlEmailPath>', 'HTML template path')
-    .option('-e, --email-list <emailListPath>', 'Email list file path')
-    .option('-n, --name <fromName>', 'Sender name')
-    .option('-s, --subject <subject>', 'Email subject')
-    .option('-a, --attachment <attachmentPath>', 'Attachment file path')
-    .option('-f, --from <fromEmail>', 'Sender email address')
-    .option('-t, --text <emailText>', 'Email text')
+  .option('-h, --html <htmlEmailPath>', 'HTML template path')
+  .option('-e, --email-list <emailListPath>', 'Email list file path')
+  .option('-n, --name <fromName>', 'Sender name')
+  .option('-s, --subject <subject>', 'Email subject')
+  .option('-a, --attachment <attachmentPath>', 'Attachment file path')
+  .option('-f, --from <fromEmail>', 'Sender email address')
+  .option('-t, --text <emailText>', 'Email text');
 
-program.parse(process.argv)
+program.parse(process.argv);
 
-console.log(program.opts()) 
+console.log(program.opts());
 
 // handle no email list path
 if (!program.opts().emailList) {
-    console.log('Please provide an email list path')
-    program.help()
-    process.exit(1)
+  console.log('Please provide an email list path');
+  program.help();
+  process.exit(1);
 }
 
 // handle no sender name
 if (!program.opts().name) {
-    console.log('Please provide a sender name')
-    program.help()
-    process.exit(1)
+  console.log('Please provide a sender name');
+  program.help();
+  process.exit(1);
 }
 
 // handle no email subject
 if (!program.opts().subject) {
-    console.log('Please provide an email subject')
-    program.help()
-    process.exit(1)
+  console.log('Please provide an email subject');
+  program.help();
+  process.exit(1);
 }
 
 // handle no sender email
 if (!program.opts().from) {
-    console.log('Please provide a sender email')
-    program.help()
-    process.exit(1)
+  console.log('Please provide a sender email');
+  program.help();
+  process.exit(1);
 }
 
 sendEmail({
-    fromEmail: program.opts().from,
-    fromName: program.opts().name,
-    subject: program.opts().subject,
-    templatePath: program.opts().html,
-    text: program.opts().text,
-    attachmentPath: program.opts().attachment,
-    emailListPath: program.opts().emailList,
+  fromEmail: program.opts().from,
+  fromName: program.opts().name,
+  subject: program.opts().subject,
+  templatePath: program.opts().html,
+  text: program.opts().text,
+  attachmentPath: program.opts().attachment,
+  emailListPath: program.opts().emailList,
 });
-
--- sendEmail.js
+++ sendEmail.js
@@ -5,24 +5,26 @@ require('dotenv').config();
 
 // exit if sendgrid api key is not set
 if (!process.env.SENDGRID_API_KEY) {
-  console.log('Please set your SENDGRID_API_KEY environment variable')
-  process.exit(1)
+  console.log('Please set your SENDGRID_API_KEY environment variable');
+  process.exit(1);
 }
 
 // Set your SendGrid API key
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 function sendEmail({
-  fromEmail ,
-  fromName ,
-  subject ,
+  fromEmail,
+  fromName,
+  subject,
   templatePath,
   attachmentPath,
   emailListPath,
-  text
+  text,
 }) {
-
-    const emails = fs.readFileSync(emailListPath, 'utf-8').split('\n').filter(email => email.trim());
+  const emails = fs
+    .readFileSync(emailListPath, 'utf-8')
+    .split('\n')
+    .filter((email) => email.trim());
 
   // Loop through each email and send it
   for (const email of emails) {
@@ -36,37 +38,37 @@ function sendEmail({
     };
 
     if (templatePath) {
-        // Add HTML content if templatePath is provided
-        const emailHTML = fs.readFileSync(templatePath, 'utf-8');
-        msg.html = emailHTML;
-      } else if (text) {
-        // Use text content if HTML is not provided but text is
-        msg.text = text;
-      }
+      // Add HTML content if templatePath is provided
+      const emailHTML = fs.readFileSync(templatePath, 'utf-8');
+      msg.html = emailHTML;
+    } else if (text) {
+      // Use text content if HTML is not provided but text is
+      msg.text = text;
+    }
 
-        // Add attachment if attachmentPath is provided
-        if (attachmentPath) {
-            const attachmentBuffer = fs.readFileSync(attachmentPath);
-            const attachmentBase64 = attachmentBuffer.toString('base64');
-            msg.attachments = [
-              {
-                content: attachmentBase64,
-                filename: 'event.ics',
-                type: 'text/calendar',
-                disposition: 'attachment',
-                contentId: 'myId',
-              },
-            ];
-          }
+    // Add attachment if attachmentPath is provided
+    if (attachmentPath) {
+      const attachmentBuffer = fs.readFileSync(attachmentPath);
+      const attachmentBase64 = attachmentBuffer.toString('base64');
+      msg.attachments = [
+        {
+          content: attachmentBase64,
+          filename: 'event.ics',
+          type: 'text/calendar',
+          disposition: 'attachment',
+          contentId: 'myId',
+        },
+      ];
+    }
 
     sgMail
       .send(msg)
       .then(() => {
-        console.log(); 
+        console.log();
         console.log(`Email sent to ${email}`);
       })
       .catch((error) => {
-        console.log(); 
+        console.log();
         console.error(`Error sending email to ${email}:`, error);
       });
   }
